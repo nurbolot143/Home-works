4fe390a4c9465118c8283b61b5459147b40d2048
Nurbolot Boobekov <nurbolot.boobekov.pls@gmail.com>
2021-06-18 19:02:27 +0300

    hw-js

---
+++ h-w_js/js/main.js
@@ -0,0 +1,12 @@
+// Работа с переменными
+let admin;
+let name;
+
+userName = "Иван";
+admin = userName;
+
+alert("Admin:" + admin);
+
+// Придумать правильные имена для переменных
+let ourPlanet;
+let currentUser;
